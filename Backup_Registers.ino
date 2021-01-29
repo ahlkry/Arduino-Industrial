7e45561ee4b1059d7e39b4200ac1014e0e0bff96
Christian Salazar <cristian.salazar.1991@gmail.com>
2021-01-29 15:01:43 -0500

    Add files via upload

---
+++ Backup_Registers.ino
@@ -0,0 +1,20 @@
+#include <libmaple/bkp.h>
+uint16_t Count;
+void setup() {
+  // put your setup code here, to run once:
+Serial1.begin(115200);
+ bkp_init(); 
+  Count= bkp_read(1);
+  Serial1.println("BCKP Registers Init");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    Count++;
+    bkp_enable_writes();        //guardamos los datos en memoria remanente batt
+    bkp_write(1,Count);
+    bkp_disable_writes();
+    Serial1.print("Cuenta es: ");
+    Serial1.println(bkp_read(1));
+    delay(500);
+}
